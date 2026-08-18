401956f0da7e0d75ba4238c704b4b09303659e2e
Mahesh Nandkishor Shingane <maheshshing@cybage.com>
2026-08-18 17:12:51 +0530

    reverted  port dynamic

--- src/app.js
+++ src/app.js
@@ -23,11 +23,9 @@ app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
 
-const PORT = process.env.PORT || 3000;
-
 connectDB().then(() => {
   console.log("connection established!!!...");
-  app.listen(PORT, () => {
-    console.log(`server is running on http://localhost:${PORT}`);
+  app.listen(3000, () => {
+    console.log("server is running on http://localhost:3000");
   });
 });
