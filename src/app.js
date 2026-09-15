254337776cc3bfff05fa64e91f2ed85378ae3f0e
Mahesh Nandkishor Shingane <maheshshing@cybage.com>
2026-09-15 15:52:21 +0530

    Refactor CORS configuration to use an array of allowed origins

--- src/app.js
+++ src/app.js
@@ -10,12 +10,22 @@ const cors = require("cors");
 
 const app = express();
 
+const allowedOrigins = [
+  "https://maheshshingane.site",
+  "https://www.maheshshingane.site",
+  "https://devtinder-web-mnl6.onrender.com",
+  "http://localhost:5173",
+];
+
 app.use(
   cors({
-    origin:
-      process.env.NODE_ENV === "production"
-        ? "https://devtinder-web-mnl6.onrender.com"
-        : "http://localhost:5173",
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   }),
 );
